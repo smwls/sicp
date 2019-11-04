b638d4d604e5464e5b27b2d2bbb4752d9c76ba6a
Samuel Walls <samuel.m.walls@gmail.com>
2019-11-05 10:27:57 +1300

    backup

--- untitled.scm
+++ untitled.scm
@@ -1752,6 +1752,48 @@ DOIN A BOOOORK
 
                                         ; HUGE MESS ABOVE BUT WHATEVS!!!!!!!
 
+(define (element-of-set? x set)
+  (cond ((null? set) false)
+        ((equal? x (car set)) true)
+        (else (element-of-set? x (cdr set)))))
 
+(define (adjoin-set x set)
+  (if (element-of-set? x set)
+      set
+      (cons x set)))
 
+(define (intersection-set set1 set2)
+  (cond ((or (null? set1) (null? set2)) '())
+        ((element-of-set? (car set1) set2)
+         (cons (car set1) (intersection-set (cdr set1) set2)))
+        (else (intersection-set (cdr set1) set2))))
 
+(define (union-set set1 set2)
+  (cond ((null? set2) set1)
+        ((null? set1) set2)
+        ((element-of-set? (car set1) set2)
+         (union-set (cdr set1) set2))
+        (else (union-set (cdr set1) (adjoin-set (car set1) set2)))))
+
+(union-set '(1 2 3 4) '(3 4 5 6))
+;ordered list representation
+(define (element-of-set? x set)
+  (cond ((null? set) false)
+        ((= x (car set)) true)
+        ((< x (car set)) false)
+        (else (element-of-set? x (cdr set)))))
+
+(define (intersection-set set1 set2)
+  (if (or (null? set1) (null? set2))
+      '()
+      (let ((x1 (car set1)) (x2 (car set2)))
+        (cond ((= x1 x2)
+               (cons x1 (intersection-set (cdr set1) (cdr set2))))
+              ((< x1 x2)
+               (intersection-set (cdr set1) set2))
+              ((> x1 x2)
+               (intersection-set set1 (cdr set2)))))))
+(intersection-set '(1 2 3 4 5) '(3 4 5 6 7))
+
+(define (adjoin-set x set)
+  (if (null? set) ))
