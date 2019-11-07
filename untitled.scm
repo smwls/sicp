95f26e5891af51a9196bedd8265c1e5159633fea
Samuel Walls <samuel.m.walls@gmail.com>
2019-11-07 22:05:00 +1300

    backup

--- untitled.scm
+++ untitled.scm
@@ -1811,3 +1811,93 @@ DOIN A BOOOORK
         ((> (car set1) (car set2)) (cons (car set2) (union-set set1 (cdr set2))))))
 
 (union-set '(1 3 6 8 9 13 16) '(4 6 9 13 17 20))
+
+(define (entry tree) (car tree))
+
+(define (left-branch tree) (cadr tree))
+
+(define (right-branch tree) (caddr tree))
+
+(define (make-tree entry left right)
+  (list entry left right))
+
+(define (element-of-set? x set)
+  (cond ((null? set) false)
+        ((= x (entry set)) true)
+        ((< x (entry set))
+         (element-of-set? x (left-branch set)))
+        ((> x (entry set))
+         (element-of-set? x (right-branch set)))))
+
+(define test-tree (make-tree 4 (make-tree 2 (make-tree 1 '() '()) (make-tree 3 '() '())) (make-tree 6 '() '())))
+
+(element-of-set? 3 test-tree)
+
+(< 5 (entry (right-branch test-tree)))
+
+(define (adjoin-set x set)
+  (cond ((null? set) (make-tree x '() '()))
+        ((= x (entry set)) set)
+        ((< x (entry set))
+         (make-tree (entry set)
+                    (adjoin-set x (left-branch set))
+                    (right-branch set)))
+        ((> x (entry set))
+         (make-tree (entry set) (left-branch set)
+                    (adjoin-set x (right-branch set))))))
+
+(adjoin-set 5 '())
+
+(define (tree->list-1 tree)
+  (if (null? tree)
+      '()
+      (append (tree->list-1 (left-branch tree))
+              (cons (entry tree)
+                    (tree->list-1
+                     (right-branch tree))))))
+
+(define (tree->list-2 tree)
+  (define (copy-to-list tree result-list)
+    (if (null? tree)
+        result-list
+        (copy-to-list (left-branch tree)
+                      (cons (entry tree)
+                            (copy-to-list
+                             (right-branch tree)
+                             result-list)))))
+  (copy-to-list tree '()))
+
+(tree->list-2 test-tree)
+
+(define (leaf x) (make-tree x '() '()))
+(define t1 (make-tree 7 (make-tree 3 (leaf 1) (leaf 5)) (make-tree 9 '() (leaf 11))))
+(define t2 (make-tree 3 (leaf 1) (make-tree 7 (leaf 5) (make-tree 9 '() (leaf 11)))))
+(define t3 (make-tree 5 (make-tree 3 (leaf 1) '()) (make-tree 9 (leaf 7) (leaf 11))))
+
+(tree->list-1 (make-tree 4 (make-tree 3 (make-tree 2 (leaf 1) '()) '()) '()))
+
+(define (list->tree elements)
+  (car (partial-tree elements (length elements))))
+(define (partial-tree elts n)
+  (if (= n 0)
+      (cons '() elts)
+      (let ((left-size (quotient (- n 1) 2)))
+        (let ((left-result
+               (partial-tree elts left-size)))
+          (let ((left-tree (car left-result))
+                (non-left-elts (cdr left-result))
+                (right-size (- n (+ left-size 1))))
+            (let ((this-entry (car non-left-elts))
+                  (right-result
+                   (partial-tree
+                    (cdr non-left-elts)
+                    right-size)))
+              (let ((right-tree (car right-result))
+                    (remaining-elts
+                     (cdr right-result)))
+                (cons (make-tree this-entry
+                                 left-tree
+                                 right-tree)
+                      remaining-elts))))))))
+
+(list->tree '(1 3 5 7 9 11))
